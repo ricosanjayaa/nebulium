d96f0962b42788add6fa2467dff1ab463c622615
ricosanjayaa <ricosanjayasan@gmail.com>
2024-08-28 08:32:34 +0700

    change email

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Footer() {
   return(
     <footer className="flex flex-col gap-12">
-      <p className="text-md sm:text-xl"><span className="opacity-50">I am looking forward on your next project. </span><Link href="mailto:ricosanjayasan@gmail.com" className="underline underline-offset-2 opacity-50 hover:opacity-100">ricosanjayasan@gmail.com</Link></p>
+      <p className="text-md sm:text-xl"><span className="opacity-50">I am looking forward on your next project. </span><Link href="mailto:ricosnjys@gmail.com" className="underline underline-offset-2 opacity-50 hover:opacity-100">ricosnjys@gmail.com</Link></p>
       <p className="text-sm sm:text-base"><span className="opacity-50">@ {new Date().getFullYear()} </span><Link href="/" className="hover:underline underline-offset-2 opacity-50 hover:opacity-100">Rico Sanjaya</Link><span className="opacity-50">, all rights reserved.</span></p>
     </footer>
   );
